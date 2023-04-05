63f2481585412330034b4cd5b71c533525dbb624
Bao Thai Le <baothai.le@dxc.com>
2023-04-05 12:55:32 +0700

    merger code

---
+++ src/main/java/com/example/test/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class HomeController {
+    @GetMapping("/")
+    public String homepage(){
+        return "index";
+    }
+    @GetMapping("/about") // Nếu người dùng request tới địa chỉ "/about"
+    public String about() {
+        return "about"; // Trả về file about.html
+    }
+    @GetMapping("/hello")
+    public String hello(
+            // Request param "name" sẽ được gán giá trị vào biến String
+            @RequestParam(name = "name", required = false, defaultValue = "") String name,
+            // Model là một object của Spring Boot, được gắn vào trong mọi request.
+            Model model
+    ) {
+        // Gắn vào model giá trị name nhận được
+        model.addAttribute("name", name);
+
+        return "hello"; // trả về file hello.html cùng với thông tin trong object Model
+    }
+
+}
